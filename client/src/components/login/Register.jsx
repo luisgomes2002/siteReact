288127208b1d9af620f4e9371e4bd9498f515c0b
Luis <luisgmgomes@gmail.com>
2023-05-09 00:23:24 -0300

    Create user 50%

--- client/src/components/login/Register.jsx
+++ client/src/components/login/Register.jsx
@@ -1,7 +1,68 @@
-import React from 'react';
+import React, { useState } from 'react';
+import NavBar from '../nav/Nav';
+import Breadcrumb from 'react-bootstrap/Breadcrumb';
+import { Link } from 'react-router-dom';
+import Axios from 'axios'
+import './Register.css';
 
 const Register = () => {
 
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [age, setAge] = useState(0)
+
+  const employee = () => {
+    Axios.post('http://localhost:3001/register',
+      {
+        name: name,
+        email: email,
+        password: password,
+        age: age
+      }).then(() => {
+        console.log('success')
+      })
+  }
+
+  return (
+    <>
+      <nav>
+        <NavBar />
+        <Breadcrumb className='bread-itens-jlpt'>
+          <Breadcrumb.Item ><Link to='/'><i className="fa-solid fa-house"></i></Link></Breadcrumb.Item>
+          <Breadcrumb.Item >Registrar</Breadcrumb.Item>
+        </Breadcrumb>
+      </nav>
+
+      <form>
+        <label>Nome:</label>
+        <input
+          type="text"
+          onChange={(event) => {
+            setName(event.target.value)
+          }} />
+        <label>Email:</label>
+        <input
+          type="email"
+          onChange={(event) => {
+            setEmail(event.target.value)
+          }} />
+        <label>Senha:</label>
+        <input
+          type="password"
+          onChange={(event) => {
+            setPassword(event.target.value)
+          }} />
+        <label>Idade:</label>
+        <input
+          type="number"
+          onChange={(event) => {
+            setAge(event.target.value)
+          }} />
+        <button className="button-confirm" onClick={employee}>CRIAR</button>
+      </form>
+    </>
+  )
 }
 
 export default Register
---
+++ server/index.js
@@ -0,0 +1,36 @@
+const express = require('express')
+const app = express()
+const mysql = require('mysql2')
+const cors = require('cors')
+
+app.use(cors())
+app.use(express.json())
+
+const db = mysql.createConnection({
+  user: 'root',
+  host: 'localhost',
+  password: 'Luisgustavo2002',
+  database: 'user accounts'
+})
+
+app.post('/register', (req, res) => {
+  const name = req.body.name
+  const email = req.body.email
+  const password = req.body.password
+  const age = req.body.age
+
+  db.query('INSERT INTO users (user_name, user_email, user_password, user_age) VALUES (?,?,?,?)',
+    [name, email, password, age],
+    (err, result) => {
+      if (err) {
+        console.log(err)
+      } else {
+        res.send('Values inserted')
+      }
+    }
+  )
+})
+
+app.listen(3001, () => {
+  console.log("Server is running on port 3001")
+})
