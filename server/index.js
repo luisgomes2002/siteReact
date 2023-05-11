0bdbafa97536fec5a443d4484aae6a44c7eb9cce
Luis <luisgmgomes@gmail.com>
2023-05-11 00:48:23 -0300

    postgre db

--- server/index.js
+++ server/index.js
@@ -1,35 +1,12 @@
 const express = require('express')
 const app = express()
-const mysql = require('mysql2')
-const cors = require('cors')
+const consign = require('consign')
 
-app.use(cors())
-app.use(express.json())
-
-const db = mysql.createConnection({
-  user: 'root',
-  host: 'localhost',
-  password: 'Luisgustavo2002',
-  database: 'user accounts'
-})
-
-app.post('/register', (req, res) => {
-  const name = req.body.name
-  const email = req.body.email
-  const password = req.body.password
-  //const age = req.body.age
-
-  db.query('INSERT INTO users (user_name, user_email, user_password) VALUES (?,?,?)',
-    [name, email, password],
-    (err, result) => {
-      if (err) {
-        console.log(err)
-      } else {
-        res.send('Values inserted')
-      }
-    }
-  )
-})
+consign()
+  .then('./config/middlewares.js')
+  .then('./api')
+  .then('./config/routes.js')
+  .into(app)
 
 app.listen(3001, () => {
   console.log("Server is running on port 3001")
